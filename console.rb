a7f80e8df5b9a349fcacadee4be6c2470e2c9333
aramsay03 <alan@aramsay.co.uk>
2020-01-31 15:21:21 +0000

    customer.rb attrs, initialize, save funtion added and customer test data added to console

---
+++ console.rb
@@ -0,0 +1,26 @@
+require('pry')
+require_relative('models/customer')
+require_relative('models/film')
+require_relative('models/ticket')
+
+# Ticket.delete_all()
+# Film.delete_all()
+# Customer.delete_all()
+
+customer1 = Customer.new({
+  "name" => "Alan",
+  "funds" => 50
+  })
+customer1.save()
+
+customer2 = Customer.new({
+  "name" => "Amanda",
+  "funds" => 60
+  })
+customer2.save()
+
+customer3 = Customer.new({
+  "name" => "Julie",
+  "funds" => 100
+  })
+customer3.save()
